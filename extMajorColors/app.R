e70f360f2139ce513d843dfba7b87802a8ed7590
mrchypark <mrchypark@gmail.com>
2017-03-28 14:01:10 +0000

    run set

--- extMajorColors/app.R
+++ extMajorColors/app.R
@@ -6,12 +6,50 @@
 #
 #    http://shiny.rstudio.com/
 #
-
+#install.packages("ggmap")
 library(shiny)
+library(shinythemes)
 library(png)
 library(RCurl)
 library(imager)
 library(markdown)
+library(ggmap)
+
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
 
 color_extractor = function(rgb_data, colors = 3){
   kmeans_result = kmeans(x = rgb_data, centers = colors)
@@ -34,77 +72,85 @@ color_extractor = function(rgb_data, colors = 3){
 }
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("주요 색상 추출기"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        
-        tabsetPanel(type = "tabs", 
-                    tabPanel("Url 입력", 
-                             br(),
-                             includeMarkdown("info.md"),
-                             br(),textInput("url",
-                                       "색상을 추출하고자 하는 사진의 url을 입력해주세요. :",
-                                       value = "https://gephi.org/images/screenshots/layout2.png"
-                             ),
-                             actionButton("do", "처리하기"))
-                    #tabPanel("Summary", fileInput('file', '색상을 추출하고자 하는 사진을 업로드해주세요.'))
-        )
-
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("pic"),
-         plotOutput("Plot")
+ui <- fluidPage(theme = shinytheme("slate"),
+  # Application title
+  titlePanel("주요 색상 추출기"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      includeMarkdown("info.md"),
+      sliderInput("N","추출할 색상 수를 선택해주세요.",min=1,max=10,value="5"),
+      tabsetPanel(type = "tabs", 
+                  
+                  tabPanel("Url 입력", 
+                           br(),textInput("url",
+                                          "색상을 추출하고자 하는 사진의 url을 입력해주세요. :",
+                                          placeholder = "https://gephi.org/images/screenshots/layout2.png",
+                                          value = ""
+                           )),
+                           #sliderInput("N","추출할 색상 수를 선택해주세요요.",min=1,max=10,value="5")),
+                  tabPanel("파일 업로드", 
+                           br(),
+                           fileInput('file', '색상을 추출하고자 하는 사진을 업로드해주세요.'))
+                           #sliderInput("N","추출할 색상 수를 선택해주세요요.",min=1,max=10,value="5"))
       )
-   )
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("Plot")
+    )
+  )
 )
 
+
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output ) {
   v <- reactiveValues(data = NULL)
   
   observeEvent(input$url, {
     v$data <- input$url
   })
   
-  # observeEvent(input$file, {
-  #   save.image(input$file,"./tmp/aoweihf.im")
-  #   v$data <- system.file("./tmp/aoweihf.im")
-  # }) 
+  observeEvent(input$file, {
+    v$data <- input$file$datapath
+  })
   
-   output$Plot <- renderPlot({
-     if (is.null(v$data)) return()
-     png = imager::load.image(v$data)
-
-     for(col in 1:3){
-       col_sub = png[, , ,col]
-       col_sub = cbind(line = nrow(col_sub):1,
-                       as.data.frame(col_sub))
-       if(col == 1){
-         col_melt = reshape2::melt(data = col_sub,
-                                   id.vars = "line")  
-       } else {
-         col_melt[, ncol(col_melt) + 1] = reshape2::melt(data = col_sub,
-                                                         id.vars = "line")[, "value"]
-       }
-     }
-     col_melt[, "variable"] = as.numeric(substr(col_melt[, "variable"], 2, 4))
-     colnames(col_melt)[3:5] = c("Red", "Green", "Blue")
-     col_melt[, "obs"] = 1:nrow(col_melt)
-
-     color_extractor(rgb_data = col_melt[3:5], colors = 5)
-   })
-   output$pic <- renderPlot({
-     if (is.null(v$data)) return()
-     png = imager::load.image(v$data)
-     plot(png)
-   })
+  observeEvent(input$N, {
+    NULL
+  })
+  
+  output$Plot <- renderPlot({
+    if (v$data=="") return()
+    png = imager::load.image(v$data)
+    p1 <- ggimage(png[,,,])
+    for(col in 1:3){
+      col_sub = png[, , ,col]
+      col_sub = cbind(line = nrow(col_sub):1,
+                      as.data.frame(col_sub))
+      if(col == 1){
+        col_melt = reshape2::melt(data = col_sub,
+                                  id.vars = "line")  
+      } else {
+        col_melt[, ncol(col_melt) + 1] = reshape2::melt(data = col_sub,
+                                                        id.vars = "line")[, "value"]
+      }
+    }
+    col_melt[, "variable"] = as.numeric(substr(col_melt[, "variable"], 2, 4))
+    colnames(col_melt)[3:5] = c("Red", "Green", "Blue")
+    col_melt[, "obs"] = 1:nrow(col_melt)
+    
+    p2<-color_extractor(rgb_data = col_melt[3:5], colors = input$N)
+    multiplot(p1, p2, cols=2)
+  })
+  
+  # output$pic <- renderPlot({
+  #   if (v$data=="") return()
+  #   #png = imager::load.image(v$data)
+  #   plot(png)
+  # })
 }
 
 # Run the application 
--- extMajorColors/info.md
+++ extMajorColors/info.md
@@ -1,1 +1,5 @@
-최적화가 되지 않아 아직 많이 느립니다. 인내심을 가지고 기다리시면 처리 됩니다. png, jpg 등 이미지라면 동작하는 것을 확인했습니다만, 문제가 있으면 [여기](https://github.com/mrchypark/shiny_extract_major_colors_form_png/issues)에 남겨주세요. 코드는 [여기](https://github.com/mrchypark/shiny_extract_major_colors_form_png)에서 확인할 수 있습니다. [원본 코드](https://github.com/encaion/encaion/blob/Visualization/extract_major_colors_form_png.R)를 제공해주신 [김승욱](https://www.facebook.com/encaion)님 감사합니다.
+ 최적화가 되지 않아 아직 많이 느립니다. 인내심을 가지고 기다리시면 처리 됩니다. png, jpg 등 이미지라면 동작하는 것을 확인했습니다만, 문제가 있으면 [여기](https://github.com/mrchypark/shiny_extract_major_colors_form_png/issues)에 남겨주세요. 코드는 [여기](https://github.com/mrchypark/shiny_extract_major_colors_form_png)에서 확인할 수 있습니다. [원본 코드](https://github.com/encaion/encaion/blob/Visualization/extract_major_colors_form_png.R)를 제공해주신 [김승욱](https://www.facebook.com/encaion)님 감사합니다.
+
+예시 그림 url : https://gephi.org/images/screenshots/layout2.png
+<br>
+<br>
